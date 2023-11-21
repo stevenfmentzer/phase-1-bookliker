5cfd2e4f6cd50fa55b85c973b43eb98b32f90d69
Steven Mentzer <stevenfmentzer@gmail.com>
2023-11-21 10:34:24 -0500

    baseline code in index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const listElement = document.getElementById("list")
+const showPanelElement = document. getElementById("show-panel")
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    fetch('http://localhost:3000/books',{
+        method : 'GET',
+        headers : {
+            'Content-Type' : 'application/json',
+        }
+        })
+    .then(res => res.json())
+    .then(bookList => {
+        bookList.forEach(book => {
+            createBookElemement(book)
+        })
+     })
+    }
+);
+
+function createBookElemement(book){
+    const newListBookElement = document.createElement("li")
+    newListBookElement.textContent = book.title
+    listElement.appendChild(newListBookElement)
+
+    book.addEventListener('click', event => {
+        displayBookElement(book)
+    }
+}
+
+function displayBookElement(book){
+    showPanelElement.src = book.img_url
+}
